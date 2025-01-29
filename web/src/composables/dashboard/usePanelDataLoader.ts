e332a2b3cad640d8957b8c8ca7e171941b56fb48
omkark06 <omkar@zinclabs.io>
2025-01-29 16:59:07 +0530

    fix: fixed circular dependency in store index.ts

--- web/src/composables/dashboard/usePanelDataLoader.ts
+++ web/src/composables/dashboard/usePanelDataLoader.ts
@@ -312,7 +312,10 @@ export const usePanelDataLoader = (
 
     state.isOperationCancelled = true;
 
-    if (isWebSocketEnabled() && state.searchRequestTraceIds) {
+    if (
+      isWebSocketEnabled(store.state) &&
+      state.searchRequestTraceIds
+    ) {
       try {
         // loop on state.searchRequestTraceIds
         state.searchRequestTraceIds.forEach((traceId) => {
@@ -1182,7 +1185,7 @@ export const usePanelDataLoader = (
                 Number(endISOTimestamp),
               );
               state.annotations = annotations;
-              if (isWebSocketEnabled()) {
+              if (isWebSocketEnabled(store.state)) {
                 await getDataThroughWebSocket(
                   query,
                   it,
